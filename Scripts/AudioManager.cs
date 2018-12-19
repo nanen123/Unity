93dfaf1c68574bed922e4ec02ec53f509bdc23f2
nanen123 <lkjhgf13579@naver.com>
2018-12-19 09:11:31 +0900

    Add AudioManager

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    public AudioClip clip;
+    public AudioSource source;
+
+    public string name;
+
+    public bool loop;
+    public float volume;
+
+    public void SetSound(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+        source.loop = loop;
+        source.volume = volume;
+    }
+
+    public void Play()
+    {
+        source.Play();
+    }
+}
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        for(int i=0;i<sounds.Length;i++)
+        {
+            GameObject soundObj = new GameObject(""+i);
+            sounds[i].SetSound(soundObj.AddComponent<AudioSource>());
+            soundObj.transform.SetParent(transform);
+        }
+    }
+
+    public void Play(string name)
+    {
+        for(int i=0;i<sounds.Length;i++)
+        {
+            if(name == sounds[i].name)
+            {
+                sounds[i].Play();
+            }
+        }
+    }
+}
