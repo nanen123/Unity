a9feb9421d1d687675a532f48e5bfa7999e476b0
nanen123 <lkjhgf13579@naver.com>
2018-12-19 09:28:07 +0900

    Add FadeOut

---
+++ Scripts/Fadeout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fadeout : MonoBehaviour {
+
+    public Color color;
+    public Image img;
+
+    private void Start()
+    {
+        StartCoroutine("FadeOut");
+    }
+
+    IEnumerator FadeOut()
+    {
+        
+        while (color.a < 1f)
+        {
+            color.a += 0.01f;
+            img.color = color;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
